e2a3867179dc4d0fb2b32581b5ace73d9ffcb9a3
Sung Ye In <syi9397@naver.com>
2020-12-30 15:47:27 +0900

    [CREATE] modules

---
+++ src/modules/header.js
@@ -0,0 +1,14 @@
+const LOGIN_ON = 'LOGIN_ON'
+const LOGIN_OUT = 'LOGIN_OUT'
+
+const initialState = {
+    login: false
+}
+
+export default function(state=initialState, action) {
+    switch(action.type) {
+        
+        default:
+            return state
+    }
+}
