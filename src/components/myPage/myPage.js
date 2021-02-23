fd9c6dd3c4a49cb1c1fddfd881e4b37b5c32f240
Sung Ye In <syi9397@naver.com>
2021-02-23 14:28:47 +0900

    FIX; button layout

--- src/components/myPage/myPage.js
+++ src/components/myPage/myPage.js
@@ -1,84 +1,83 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios'
+import axios from "axios";
 
-import { useCookies } from 'react-cookie'
+import { useCookies } from "react-cookie";
 
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
-import RegisterModal from './registerModal'
+import RegisterModal from "./registerModal";
 
-import * as S from './styles'
+import Header from "../Public/Header/Header";
+
+import * as S from "./styles";
 
 const MyPage = () => {
-    const [infor, setInfor] = useState([])
-    const [Acookie] = useCookies(['access-token'])
-    const history = useHistory()
-    const [modal, modalOn] = useState(false);
+  const [infor, setInfor] = useState([]);
+  const [Acookie] = useCookies(["access-token"]);
+  const history = useHistory();
+  const [modal, modalOn] = useState(false);
 
-    // 마이페이지 토큰 없으면 접근 불가....
-    useEffect(() => {
-        if (!Acookie['access-token']) {
-            history.push('/')
-        }
-
-        axios({
-            method: 'get',
-            url: '/myservice',
-            headers: {
-                'access-token': `Bearer ${Acookie['access-token']}`
-            }
-        })
-            .then(res => {
-                console.log(res)
-                setInfor(res.data)
-            })
-            .catch(err => {
-                console.log(err.response)
-            })
-    }, [])
-
-    const ModalOn = () => {
-        modalOn(!modal);
+  // 마이페이지 토큰 없으면 접근 불가....
+  useEffect(() => {
+    if (!Acookie["access-token"]) {
+      history.push("/");
     }
 
-    return (
-        <>
-            <RegisterModal
-                style={modal}
-                setModalOn={ModalOn}
-            />
-            <S.Wrapper>
-                <S.MyContainer>
-                    <S.UserName>김팔복<b>1320</b></S.UserName>
-                    <S.UserEmail>jidole041214@naver.com</S.UserEmail>
-                    <S.UserApp>
-                        내가 등록한 애플리케이션
-                    <S.AddApp
-                        onClick={ModalOn}
-                    >+</S.AddApp>
-                    </S.UserApp>
-                    {
-                        infor.length === 0 && <S.NoneApp>아직 등록된 애플리케이션이 없습니다!</S.NoneApp>
-                    }
+    axios({
+      method: "get",
+      url: "/myservice",
+      headers: {
+        "access-token": `Bearer ${Acookie["access-token"]}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        setInfor(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
 
-                    {
-                        infor.map(({ name, domain_url, redirect_url, client_secret },index) => (
-                            <S.Application key={index}>
-                                <p>{name}</p>
-                                <S.AppDetail>
-                                    <p>도메인 : {domain_url}</p>
-                                    <p>리다이렉트 도메인 : {redirect_url}</p>
-                                    <p>서비스 시크릿 코드 : {client_secret}</p>
-                                </S.AppDetail>
-                            </S.Application>
-                        ))
-                    }
+  const ModalOn = () => {
+    modalOn(!modal);
+  };
 
-                </S.MyContainer>
-            </S.Wrapper>
-        </>
-    )
-}
+  return (
+    <>
+      <RegisterModal style={modal} setModalOn={ModalOn} />
+      <Header />
+      <S.Wrapper>
+        <S.MyContainer>
+          <S.UserName>
+            김팔복<b>1320</b>
+          </S.UserName>
+          <S.UserEmail>jidole041214@naver.com</S.UserEmail>
+          <S.UserApp>
+            내가 등록한 애플리케이션
+            <S.AddApp onClick={ModalOn}>+</S.AddApp>
+          </S.UserApp>
+          {infor.length === 0 && (
+            <S.NoneApp>아직 등록된 애플리케이션이 없습니다!</S.NoneApp>
+          )}
 
-export default MyPage
+          {infor.map(
+            ({ name, domain_url, redirect_url, client_secret }, index) => (
+              <S.Application key={index}>
+                <p>{name}</p>
+                <S.AppDetail>
+                  <p>도메인 : {domain_url}</p>
+                  <p>리다이렉트 도메인 : {redirect_url}</p>
+                  <p>서비스 시크릿 코드 : {client_secret}</p>
+                </S.AppDetail>
+              </S.Application>
+            )
+          )}
+        </S.MyContainer>
+      </S.Wrapper>
+    </>
+  );
+};
+
+export default MyPage;
--- src/components/myPage/styles.js
+++ src/components/myPage/styles.js
@@ -1,167 +1,171 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width:100%;
-    height:100vh;
-    min-height:500px;
-    background:linear-gradient(#151717,black);
-    overflow-y:scroll;
-    min-width:1900px;
-`
+  width: 100%;
+  height: 100vh;
+  min-height: 500px;
+  background: linear-gradient(#151717, black);
+  overflow-y: scroll;
+  min-width: 1900px;
+`;
 
 export const MyContainer = styled.div`
-    padding:6% 20%;
-`
+  padding: 6% 20%;
+`;
 
 export const UserName = styled.p`
-    color:white;
-    font-family:나눔스퀘어;
-    font-size:6vmin;
-    margin-bottom:0;
-    b{
-        font-size:2.5vmin;
-        padding: 0 10px;
-        color:#C4C4C4;
-        font-weight:500;
-    }
-`
+  color: white;
+  font-family: 나눔스퀘어;
+  font-size: 6vmin;
+  margin-bottom: 0;
+  b {
+    font-size: 2.5vmin;
+    padding: 0 10px;
+    color: #c4c4c4;
+    font-weight: 500;
+  }
+`;
 
 export const UserEmail = styled.p`
-    color:#C4C4C4;
-    font-size:2.3vmin;
-    letter-spacing:1px;
-`
+  color: #c4c4c4;
+  font-size: 2.3vmin;
+  letter-spacing: 1px;
+`;
 
 export const UserApp = styled.div`
-    display:flex;
-    color:white;
-    margin-top:7%;
-    font-size:3vmin;
-    align-items:center;
-`
+  display: flex;
+  color: white;
+  margin-top: 7%;
+  font-size: 3vmin;
+  align-items: center;
+`;
 
 export const AddApp = styled.button`
-    width:5vmin;
-    height:5vmin;
-    margin-left:1%;
-    cursor:pointer;
-    background-color:#713EFF;
-    border:none;
-    border-radius:3px;
-    color:white;
-    font-size:4vmin;
-    outline:none;
-`
+  width: 5vmin;
+  height: 5vmin;
+  margin-left: 1%;
+  cursor: pointer;
+  background-color: #713eff;
+  border: none;
+  border-radius: 3px;
+  color: white;
+  font-size: 4vmin;
+  outline: none;
+  line-height: 4.3vmin;
+`;
 
 export const NoneApp = styled.div`
-    width:50%;
-    padding:2% 3%;
-    background-color:rgb(50,50,50,0.4);
-    margin-top:5%;
-    border-radius:15px;
-    color:#B1B1B1;
-    font-family:나눔스퀘어;
-    font-size:2vmin;
-`
+  width: 50%;
+  padding: 2% 3%;
+  background-color: rgb(50, 50, 50, 0.4);
+  margin-top: 5%;
+  border-radius: 15px;
+  color: #b1b1b1;
+  font-family: 나눔스퀘어;
+  font-size: 2vmin;
+`;
 
 export const ModalWrapper = styled.div`
-    width:100%;
-    height:100%;
-    position:absolute;
-    background-color:rgb(10,10,10,0.7);
-    z-index:200;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    min-height:500px;
-`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: rgb(10, 10, 10, 0.7);
+  z-index: 200;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 500px;
+`;
 
 export const ModalContainer = styled.div`
-    width:30%;
-    height:45%;
-    background-color:white;
-    border-radius:10px;
-    padding:1% 2%;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    :first-child{
-        font-size:2.5vmin;
-        font-weight:bold;
-        font-family:나눔스퀘어;
-    }
-`
+  width: 30%;
+  height: 45%;
+  background-color: white;
+  border-radius: 10px;
+  padding: 1% 2%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  :first-child {
+    font-size: 2.5vmin;
+    font-weight: bold;
+    font-family: 나눔스퀘어;
+  }
+`;
 
 export const ModalLine = styled.div`
-    width:100%;
-    height:1px;    
-    background-color:#B7B7B7;
-    margin-top:4%;
-    :first-of-type{
-        margin-top:0;
-    }
-`
+  width: 100%;
+  height: 1px;
+  background-color: #b7b7b7;
+  margin-top: 4%;
+  :first-of-type {
+    margin-top: 0;
+  }
+`;
 
 export const InputApp = styled.input`
-    width:80%;
-    height:12%;
-    border:1px solid #828282;
-    border-radius:3px;
-    margin-top:3%;
-    padding:0 4%;
-    color:gray;
-    font-weight:bold;
-    font-size:1.8vmin;
-    font-family:나눔스퀘어;
-    outline:none;
-    ::placeholder{
-        color:#BFBFBF;
-        font-weight:bold;
-        font-size:1.8vmin;
-        font-family:나눔스퀘어;
-    }
-`
+  width: 80%;
+  height: 12%;
+  border: 1px solid #828282;
+  border-radius: 3px;
+  margin-top: 3%;
+  padding: 0 4%;
+  color: gray;
+  font-weight: bold;
+  font-size: 1.8vmin;
+  font-family: 나눔스퀘어;
+  outline: none;
+  ::placeholder {
+    color: #bfbfbf;
+    font-weight: 500;
+    font-size: 1.8vmin;
+    font-family: "Noto Sans KR", sans-serif;
+  }
+`;
 
 export const BtnCont = styled.div`
-    margin-top:5%;
-    width:50%;
-    display:flex;
-    justify-content:space-between;
-    height:9%;
-    button{
-        width:43%;
-        height:100%;
-        background-color:white;
-        cursor:pointer;
-        outline:none;
-    }
-`
+  margin-top: 5%;
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  height: 9%;
+  & button {
+    width: 43%;
+    height: 100%;
+    background-color: white;
+    cursor: pointer;
+    outline: none;
+  }
+  & button:hover {
+    filter: brightness(0.9);
+  }
+`;
 
 export const Application = styled.div`
-    width:100%;
-    margin-top:3%;
-    padding:1% 3%;
-    background-color:rgb(50,50,50,0.5);
-    color:white;
-    font-size:3vmin;
-    border-radius:5px;
-    font-weight:bold;
-    font-family:나눔스퀘어;
-    p{
-        margin-top:1.5%;
-        margin-bottom:0;
-    }
-`
+  width: 100%;
+  margin-top: 3%;
+  padding: 1% 3%;
+  background-color: rgb(50, 50, 50, 0.5);
+  color: white;
+  font-size: 3vmin;
+  border-radius: 5px;
+  font-weight: bold;
+  font-family: 나눔스퀘어;
+  p {
+    margin-top: 1.5%;
+    margin-bottom: 0;
+  }
+`;
 
 export const AppDetail = styled.div`
-    width:100%;
-    display:flex;
-    justify-content:space-between;
-    font-size:1.8vmin;
-    color:#A4A4A4;
-    font-weight:500;
-    p{
-        margin-top:2%;
-        margin-bottom:2%;
-    }
-`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.8vmin;
+  color: #a4a4a4;
+  font-weight: 500;
+  p {
+    margin-top: 2%;
+    margin-bottom: 2%;
+  }
+`;
