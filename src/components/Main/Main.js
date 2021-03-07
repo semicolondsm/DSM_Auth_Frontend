d0622cc1ce3ee9fc1829cdefb22c810ddf8f45bb
Sung Ye In <syi9397@naver.com>
2021-03-07 09:54:54 +0900

    FIX; remove session storage

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -69,6 +69,8 @@ const Main = (props) => {
   const LogOut = () => {
     Aremove("access-token");
     Rremove("refresh-token");
+    sessionStorage.removeItem("infor");
+    sessionStorage.removeItem("services");
 
     setLogin(false);
 
